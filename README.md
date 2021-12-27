929fea7c0a1ffa4fd3fb5168398040f928912e14
Lexxrt <70237512+Lexxrt@users.noreply.github.com>
2021-12-27 22:59:31 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+<h2><strong>xNft</strong></h2>
+
+- _What is xNft?_
+
+`xNft is a Cyberpunk NFT scraper, that will fetch you all 10,000 NFTs.`
+
+
+> 1) Install Python 3
+> 2) Install the libraries `python install -r requirements.txt`
+> 3) Run the script: `./xNft.py `
+
+- Images:
+
+<img src="pic.jpg">
+
+<hr>
+
+<img src="pic2.jpg">
+
+- Enjoy ;)
+
---
+++ xNft.py
@@ -0,0 +1,46 @@
+import os
+import requests
+import colorama
+
+colorama.init(convert=True)
+
+R = colorama.Fore.RED
+G = colorama.Fore.GREEN
+B = colorama.Fore.BLUE
+C = colorama.Fore.CYAN
+X = colorama.Fore.RESET
+
+def main():
+    print(f'''{C}
+              _   _  __ _   
+             | \ | |/ _| |  
+        __  _|  \| | |_| |_ 
+        \ \/ / . ` |  _| __|
+         >  <| |\  | | | |_ 
+        /_/\_\_| \_|_|  \__|
+
+        Author: Lexxrt
+        Github: https://github.com/Lexxrt
+    {X}''')
+
+    for code in range(0, 10000):
+        filename = f'cryptopunk{str(code).zfill(4)}.png'
+        with open(f'./output/{filename}', 'wb') as out:
+            try:
+                data = requests.get(f'https://www.larvalabs.com/cryptopunks/cryptopunk{str(code).zfill(4)}.png').content
+                out.write(data)
+                print(f'{B}[+] Getting: https://www.larvalabs.com/cryptopunks/cryptopunk{str(code).zfill(4)}.png{X}')
+            except Exception as ex:
+                print('',ex)
+                print(f'{R}[!] Failed: https://www.larvalabs.com/cryptopunks/cryptopunk{str(code).zfill(4)}.png{X}')
+
+if __name__ == '__main__':
+    try:
+        if os.path.exists('./output'):
+            pass
+        else:
+            os.mkdir('output')
+
+        main()
+    except:
+        pass
